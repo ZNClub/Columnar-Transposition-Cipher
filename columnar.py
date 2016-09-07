2eaccdea057e1f9246fb472a473ba59400adad05
Nevil Dsouza <dsouza.nevil45@gmail.com>
2016-09-07 10:33:05 +0530

    Update columnar.py

--- columnar.py
+++ columnar.py
@@ -1,1 +1,19 @@
 
+#####   Implement Single & Double Columnar Cipher
+####    
+###     Version 1.0 - Nevil Dsouza
+##
+
+pt="Cryptography"
+#py=input("Enter PLAIN TEXT":)
+
+key="znevzz"
+#key=input("Enter CIPHER KEY":)
+
+chars=list(key)
+
+s_chars=sorted(chars)
+
+#print(chars,s_chars)
+
+
